ee24a9828cf87d16ffb4d2943d93de9607a54215
gsmerlin <gabrielsmerlin@outlook.com>
2021-04-13 14:37:18 -0300

    Initial Commit

---
+++ loop.js
@@ -0,0 +1,36 @@
+// node myFile.js
+
+const pendingTimers = [];
+const pendingTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFile running
+myFile.runContents();
+
+const shouldContinue = () => {
+    // Check one -> Any pending timeout, interval ou immediate
+    // Check two -> Any pending OS tasks? (Server listening to port)
+    // Check three -> Any pending long running operations? (like FS modules)
+    return pendingTimers.length || pendingTasks.length || pendingOperations.length;
+}
+
+// 1 iteration = 1 tick
+while (shouldContinue()) {
+    // Node looks at pending timers and sees if anything
+    // should be called
+
+    // Node looks at pending tasks and pending operations and
+    // executes callbacks
+
+
+    // Pauses execution. Continue when...
+    // - New Task is done
+    // - New Operation is done
+    // - A timer is about to complete
+
+    // Looks at pending timers for setImmediate exclusively
+
+    // Handle 'close' events
+};
+
+// Exits to terminal
